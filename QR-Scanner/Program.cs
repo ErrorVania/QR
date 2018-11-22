66b714ebcc0fbbc125b50eb84395e2fb4cde357b
ErrorVania <44887293+ErrorVania@users.noreply.github.com>
2018-11-22 10:16:58 +0100

    Add files via upload

---
+++ QR-Scanner/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QR_Scanner
+{
+    class Program
+    {
+        public string[] credit_to_following_APIs =
+        {
+            "https://github.com/codebude/QRCoder",
+            "http://www.barcodelib.com/net_barcode_reader/barcodes/qrcode.html"
+
+        };
+
+
+        static void Main(string[] args)
+        {
+            int i = -1;
+            Console.WriteLine("##########-Menu-##########");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("[{0}]", ++i);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(": {0}", "Scan QR-Code Image (Decode Text)");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("[{0}]", ++i);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(": {0}", "Generate QR-Code Image (Encode Text)");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("[{0}]", ++i);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(": {0}", "Generate QR-Wifi-Payload Image (Encode Wifi Credentials)");
+            ConsoleKeyInfo inf = Console.ReadKey();
+            char response = inf.KeyChar;
+            Console.Clear();
+            responseaction(response);
+            Console.ReadLine();
+        }
+        public static void responseaction(char input)
+        {
+            if (input == '0')
+            {
+                Console.WriteLine("Chosen Action: {0}", input);
+                Console.Write("Enter Image File Path to read from: ");
+                string readingfile = Console.ReadLine();
+                try
+                {
+                    apiclass.printArr(apiclass.scanQR(readingfile));
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            if (input == '1')
+            {
+                Console.WriteLine("Chosen Action: {0}", input);
+                Console.Write("Enter Text to encode to QR-Code Image: ");
+                string texttoencode = Console.ReadLine();
+                Console.Write("Enter Output File Path: ");
+                string outfile = Console.ReadLine();
+                try
+                {
+                    apiclass.generateQRcode(texttoencode).Save(outfile);
+                } catch (Exception ex) { ex.ToString(); }
+            }
+            if (input == '2')
+            {
+                Console.WriteLine("Chosen Action: {0}", input);
+                Console.WriteLine("Enter Wifi SSID: ");
+                string ssid = Console.ReadLine();
+                Console.WriteLine("Enter Wifi Password (WPA): ");
+                string pass = Console.ReadLine();
+                Console.WriteLine("Enter Output File Path: ");
+                string path = Console.ReadLine();
+                try
+                {
+                    apiclass.QRpayload_Wifi(ssid, pass).Save(path);
+                } catch (Exception ex) { ex.ToString(); }
+            }
+        }
+    }
+}
---
+++ QR-Scanner/apiclass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BarcodeLib.BarcodeReader;
+using QRCoder;
+
+namespace QR_Scanner
+{
+    class apiclass
+    {
+        public static QRCodeGenerator qrGenerator = new QRCodeGenerator();
+
+        public static string[] scanQR(string filepath, int mode = 9)
+        {
+            return BarcodeReader.read(filepath, BarcodeReader.QRCODE);
+        }
+        public static Bitmap generateQRcode(string texttoencode, int pixelspermodule = 20)
+        {
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(texttoencode, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(pixelspermodule);
+            return qrCodeImage;
+        }
+
+        public static void printArr(string[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+        public static Bitmap QRpayload_Wifi(string ssid, string pass, int pixelspermodule = 20)
+        {
+            PayloadGenerator.WiFi wifiPayload = new PayloadGenerator.WiFi(ssid, pass, PayloadGenerator.WiFi.Authentication.WPA);
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(wifiPayload.ToString(), QRCodeGenerator.ECCLevel.Q);
+
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(pixelspermodule);
+            return qrCodeImage;
+        }
+    }
+}
